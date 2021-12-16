1919aa9ca0416198bb66193dbd27ee580c1b56fd
PrinceOfDarkness-123 <94347356+PrinceOfDarkness-123@users.noreply.github.com>
2021-12-16 13:49:11 +0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FirstAngularProject';
+  title2 = 'Event Binding in Angular';
+  title3 = 'Using Multiple Events';
+  title4 = 'Using Event Payload';
+  title5 = 'Using Template Reference Variable';
+  title6 = 'Two Way Binding in Angular';
+  clickCount=0;
+  clickCount2=0;
+  clickCount3=3;
+
+  value=""
+  val=""
+handleInput(event: any) {
+  this.value = (event.target as HTMLInputElement).value;
+}
+handleInput1(element: any){
+this.val = element.value;
+}
+  clickMe(){
+    this.clickCount++;
+  }
+  clickMe2(){
+    this.clickCount3++;
+  }
+
+  name="";
+  clearName(){
+    this.name="";
+  }
+
 }
