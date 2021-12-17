f4922250ec99e1b99135c8eef8233702086bec25
PrinceOfDarkness-123 <94347356+PrinceOfDarkness-123@users.noreply.github.com>
2021-12-17 14:09:59 +0500

    Adding custom components declarations
    Angular added custom components SecondComponent and MyPersonal to the app.module file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SecondComponentComponent } from './second-component/second-component.component';
+import { MyPersonalComponent } from './my-personal/my-personal.component';
+import { FormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SecondComponentComponent,
+    MyPersonalComponent
+    
   ],
   imports: [
     BrowserModule,
