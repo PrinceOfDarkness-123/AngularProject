e8a01b51eece24fc9e77ff641cafd80f885b42a1
PrinceOfDarkness-123 <94347356+PrinceOfDarkness-123@users.noreply.github.com>
2021-12-17 16:53:12 +0500

    Import ng if component in app-module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { SecondComponentComponent } from './second-component/second-component.component';
 import { MyPersonalComponent } from './my-personal/my-personal.component';
 import { FormsModule } from '@angular/forms';
+import { NgIfDirectiveComponent } from './ng-if-directive/ng-if-directive.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SecondComponentComponent,
-    MyPersonalComponent
+    MyPersonalComponent,
+    NgIfDirective
     
   ],
   imports: [
